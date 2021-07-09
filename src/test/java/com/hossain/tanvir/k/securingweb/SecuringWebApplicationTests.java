134dd69e5869153564e813eb766ee64c9818e713
K. Tanvir Hossain <tanvir@cokreates.com>
2021-07-10 04:00:47 +0600

    init

---
+++ src/test/java/com/hossain/tanvir/k/securingweb/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hossain.tanvir.k.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
